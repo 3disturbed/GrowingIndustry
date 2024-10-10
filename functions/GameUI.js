235827358dc469142bf5f22f60f49780eacc7c8b
Darklington <codemonkey@3disturbedmonkeys.co.uk>
2024-10-10 03:18:38 +0100

    fixes UI Text

--- functions/GameUI.js
+++ functions/GameUI.js
@@ -1,5 +1,7 @@
 
 export function DrawUI(G){
+    G.ctx.fillStyle = "LightBlue";
+    G.ctx.font = "33px Arial";
     G.ctx.clearRect(0, 0, G.width, G.height);
     //draw Backdrop image
     if(G.backDropImg.complete){
@@ -14,7 +16,7 @@ export function DrawUI(G){
     G.ctx.textAlign = "left";
     G.ctx.fillText(G.user.money, 1390, 45);
     G.ctx.fillText("Pots: " + G.user.pots, G.width/6, G.height - 25);
-    G.ctx.fillText("Greenhouses: " + G.user.greenhouses.length, G.width/8, 50);
+    G.ctx.fillText("Greenhouses: " + G.user.greenhouses.length, G.width/10, 50);
     G.ctx.fillText("Market Stalls: " + G.user.marketStalls.length, G.width/8 *2, 50);
     G.ctx.fillText("Game Speed: " + G.user.gameSpeed, G.width/8*6, G.height - 25);
     if(G.showFPS){
@@ -24,8 +26,9 @@ export function DrawUI(G){
     let mx = G.mouseX;
     let my = G.mouseY;
     if(G.user.holding == 'planter'){
-        G.ctx.fillText("Planter", mx, my);
         G.ctx.drawImage(G.planterstack.img, mx-75, my-100, 150, 200);
+        G.ctx.fillText("Planter", mx-60, my+110);
+        
     }
     
 }
